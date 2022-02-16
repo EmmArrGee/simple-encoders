0135f83fa6663b4f8fd6a4afc3e63d08a87b77ce
BOC-Group/fmargrei <Fabian.Margreiter@boc-group.com>
2022-02-16 22:15:46 +0100

    Created files necessary for distributing package

---
+++ simple_encoders/__init__.py
@@ -0,0 +1,11 @@
+from encoders.base_encoder import BaseEncoder
+from encoders.df_encoder import DFEncoder
+
+__version__ = '0.1.0'
+
+__author__ = "fabian margreiter"
+
+__all__ = [
+    'BaseEncoder',
+    'DFEncoder'
+]
